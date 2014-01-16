11c4fccd9613f89a52eb89aa2c8de6d363afb4e3
fawnwong <fawn.wong@live.com>
2014-01-16 09:27:48 -0500

    K

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Game {
-	public static void main (String[] args) {
-		BlackJackGui gui = new BlackJackGui();
-	}
+    public static void main (String[] args) {
+	BlackJackGui gui = new BlackJackGui();
+    }
 }
--- Hand.java
+++ Hand.java
@@ -2,7 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Hand{
+    private ArrayList<Card> cards = new ArrayList<Card>;
     private int ptTotal;
+    
+    public int checkPtTotal(){
+	for (int i = 0; i < cards.size(); i++){
+	    cards.get(i);
+	}
+    }
     
 
 }
