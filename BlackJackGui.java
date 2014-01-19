c25403afff0ac8e18bed813daa45caedab8642c0
fawnwong <fawn.wong@live.com>
2014-01-19 14:14:13 -0500

    gui framework finished

--- BlackJackGui.java
+++ BlackJackGui.java
@@ -8,97 +8,111 @@ import javax.imageio.*;
 
 public class BlackJackGui extends JPanel implements ActionListener{
 	protected JFrame frame = new JFrame();
+	protected Container cPane = frame.getContentPane();
 	protected JPanel center = new JPanel();
 	protected JPanel dealerCards = new JPanel(new GridBagLayout());
-	protected JPanel deckPanel = new JPanel(new GridBagLayout());
-	protected JPanel leftPlayerCards = new JPanel(new GridBagLayout());
-	protected JPanel rightPlayerCards = new JPanel(new GridBagLayout());
-	protected JPanel userCards = new JPanel(new GridBagLayout());
+	protected JPanel leftPlayerCards = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+	protected JPanel rightPlayerCards = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+	protected JPanel userCards = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+	protected JPanel userThings  = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+	protected JPanel userPanel  = new JPanel(new GridBagLayout());
 	protected JButton hitButton;
     protected JButton dealButton;
   	protected JButton stayButton;
-  	protected BufferedImage deckSpriteSheet, cardBack;
+  	protected BufferedImage deckSpriteSheet;
   	protected BufferedImage[][] deck;
+  	protected JLabel[] backs = new JLabel[10]; //because swing components can't be reused...
 
+	public void actionPerformed(ActionEvent e){ 
+		if (e.getSource() == dealButton){
+			center.remove(dealButton);
+			hitButton.addActionListener(this);
+			stayButton.addActionListener(this);
+			userCards.removeAll();
+			userCards.add(getRandomCard());
+			userCards.add(getRandomCard());
+			rightPlayerCards.add(getRandomCard());
+			leftPlayerCards.add(getRandomCard());
+			dealerCards.add(getRandomCard());
+			cPane.revalidate();
+			cPane.repaint();
+		}
+		if (e.getSource() == hitButton){
+			userCards.add(getRandomCard());
+			dealerCards.add(getRandomCard());
+			rightPlayerCards.add(getRandomCard());
+			leftPlayerCards.add(getRandomCard());
+			cPane.revalidate();
+			cPane.repaint();
+		}
+	}
 
 	public BlackJackGui() {
+		createScreen(frame);
+	}
+
+	public void createScreen(JFrame frame) {
 		loadImages();
 
 		//builds screen
-		Container cPane = frame.getContentPane();
 		GridBagConstraints c = new GridBagConstraints();
 		c.weightx = 1; c.weighty = 1;
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		cPane.setBackground(new Color(52,99,13));
 		cPane.setLayout(new GridBagLayout());
-		frame.setSize(1000, 1000);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH); 
 		frame.setVisible(true);
 		frame.setResizable(false);
 
 		hitButton = new JButton("HIT");
-		hitButton.addActionListener(this);
-    	dealButton = new JButton("DEAL");
-    	dealButton.addActionListener(this);
   		stayButton = new JButton("STAY");
-  		stayButton.addActionListener(this);
-
-		dealerCards.add(getRandomCard());
-		deckPanel.add(new JLabel(new ImageIcon(cardBack)));
-		leftPlayerCards.add(getRandomCard());
-		rightPlayerCards.add(getRandomCard());
-		userCards.add(getRandomCard());
-
-		/*
-		dealerCards.
-		deckPanel.
-		leftPlayerCards.
-		rightPlayerCards.
-		userCards.
-		*/
+  		dealButton = new JButton("DEAL");
+    	dealButton.addActionListener(this);
 
 		dealerCards.setOpaque(false);
-		deckPanel.setOpaque(false);
 		leftPlayerCards.setOpaque(false);
 		rightPlayerCards.setOpaque(false);
 		userCards.setOpaque(false);
+		userThings.setOpaque(false);
+		userPanel.setOpaque(false);
 		center.setOpaque(false);
 
-        c.anchor = GridBagConstraints.LINE_END;
-		c.weightx = 0;
-		c.gridx = 1;
-        frame.add(dealButton, c);
+		dealerCards.add(backs[0]);
+		leftPlayerCards.add(backs[1]);
+		rightPlayerCards.add(backs[2]);
+		center.add(dealButton);
+
+		userThings.add(hitButton,c);
+		userThings.add(stayButton,c);
+		userCards.add(backs[3]);
+		c.gridwidth = 1;
+		c.anchor = GridBagConstraints.PAGE_END;
+		userPanel.add(userThings, c);
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridy = 1;
+		userPanel.add(userCards, c);
+
+
+
         c.anchor = GridBagConstraints.CENTER;
-        c.weightx = 1;
-        c.gridx = 2;
+        c.weightx = 1;c.gridwidth = 4;
+        c.gridx = 0;c.gridy = 0;
 		frame.add(dealerCards, c);
-        c.gridx = 3; c.gridwidth = 2;
-        c.gridy = 0;
-        frame.add(deckPanel, c);
         c.gridx = 0; c.gridwidth = 1;
         c.gridy = 1;
         frame.add(leftPlayerCards, c); 
-        c.gridx = 1; c.gridwidth = 3;      
+        c.gridx = 1; c.gridwidth = 2;      
         frame.add(center, c);      
-        c.gridx = 4; c.gridwidth = 1;
+        c.gridx = 3; c.gridwidth = 1;
         frame.add(rightPlayerCards, c);
-        c.anchor = GridBagConstraints.LINE_END;
-		c.weightx = 0;
-		c.gridx = 1;
-        c.gridy = 2;
-        frame.add(hitButton, c);
         c.anchor = GridBagConstraints.CENTER;
-        c.weightx = 1;
-        c.gridx = 2;
-        frame.add(userCards, c);
-        c.anchor = GridBagConstraints.LINE_START;
-		c.weightx = 0;
-		c.gridx = 3;
-        frame.add(stayButton, c);
-
-
+        c.weightx = 1;c.gridwidth = 4;
+        c.gridx = 0;
+        c.gridy = 2;
+        frame.add(userPanel, c);
 	}
-
+	
 	public JLabel getRandomCard() {
 		Random r = new Random();
 		int row = r.nextInt(4);
@@ -106,10 +120,18 @@ public class BlackJackGui extends JPanel implements ActionListener{
 		return new JLabel(new ImageIcon(deck[row][col]));
 	}
 
+	public JLabel getCardBack() {
+		JLabel l = new JLabel();
+		try {
+			l = new JLabel(new ImageIcon(ImageIO.read(new File("back.png"))));
+		}
+		catch (Exception e) {
+	    }
+		return l;
+	}
 	public void loadImages() {
 		try {
 			deckSpriteSheet = ImageIO.read(new File("deck.png")); 
-			cardBack = ImageIO.read(new File("back.png"));
 			deck = new BufferedImage[4][13];
 			for (int i = 0; i < 4; i++)  {  
 	            for (int j = 0; j < 13; j++)  {  
@@ -119,7 +141,8 @@ public class BlackJackGui extends JPanel implements ActionListener{
 	    }
 	    catch (Exception e) {
 	    }
-	}
-	public void actionPerformed(ActionEvent e){ 
+	    for (int i = 0; i < backs.length; i++) {
+	    	backs[i] = getCardBack();
+	    }
 	}
 }
